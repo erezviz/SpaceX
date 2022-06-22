ecc1e2e9ff95076ce57c2acbee68f6725d40f518
erezviz <erezviz@gmail.com>
2022-06-22 17:02:16 +0300

    Last commit (17:00): Routing works back and forth to and from details. some data is shown in details. Filtering hasn't been started yet.

--- src/pages/landing-details.jsx
+++ src/pages/landing-details.jsx
@@ -1,29 +1,44 @@
 import { useEffect, useState } from "react"
-import {useParams} from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
 import { landingService } from "../services/landings.service"
 export const LandingDetails = (props) => {
     const [landing, setLanding] = useState(null)
     const params = useParams()
-
+    const navigate = useNavigate()
     useEffect(() => {
         const {id} = params
         loadLanding(id)
-        console.log(id);
-        console.log(landing);
-     
+        
     }, [])
 
     const loadLanding = async (id) =>{
         const landing = await landingService.getById(id)
         setLanding(landing)
     }
-    const bcgImg = () =>{
 
-    }
+
     
-
     if(!landing) return <>Loading...</>
-    return <section className="landing-details">
+    const {small} = landing.links.patch
+    return <section style={{backgroundImage: `url(${small})`}} className="landing-details flex-col ">
        <h1>{landing.name}</h1>
+       <h3>Landing Failures:</h3>
+       <div className="failures">
+        {landing.failures.length > 0 &&
+             landing.failures.map(fail => <ul key={fail.time}>
+                <li>Altitude: {fail.altitude}</li>
+                <li>Air time: {fail.time} seconds</li>
+                <li>Reason: {fail.reason}</li>
+
+             </ul>)
+        }
+        {!landing.failures.length && <p>Succeeded</p>}
+       </div>
+       <a className="wiki-link" target="_blank" href={landing.links.wikipedia}>Wikipedia Link</a>
+       <article className="summary">
+        <p className="summary-p">{landing.details}</p>
+        <button onClick={()=> navigate(-1)}>Back</button>
+
+       </article>
     </section>
 }
